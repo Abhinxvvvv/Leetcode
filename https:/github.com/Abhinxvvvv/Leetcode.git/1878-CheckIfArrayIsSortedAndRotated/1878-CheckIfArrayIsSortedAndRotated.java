be853b1c0c4690ed23292a3156660587c3b3f4fe
Abhinav Rathore <abhinavrathore9399@gmail.com>
2026-08-09 00:06:31 +0530

    Create 1878-CheckIfArrayIsSortedAndRotated

---
+++ https:/github.com/Abhinxvvvv/Leetcode.git/1878-CheckIfArrayIsSortedAndRotated/1878-CheckIfArrayIsSortedAndRotated.java
@@ -0,0 +1,21 @@
+// Last updated: 09/08/2026, 00:06:31
+class Solution {
+    public boolean check(int[] nums) {
+        int n = nums.length;
+        int drops = 0;
+        
+        for (int i = 0; i < n; i++) {
+            // Check if current element is greater than the next element (with wrap-around)
+            if (nums[i] > nums[(i + 1) % n]) {
+                drops++;
+                
+                // CRITICAL OPTIMIZATION: Early exit if more than 1 drop is found
+                if (drops > 1) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+}
