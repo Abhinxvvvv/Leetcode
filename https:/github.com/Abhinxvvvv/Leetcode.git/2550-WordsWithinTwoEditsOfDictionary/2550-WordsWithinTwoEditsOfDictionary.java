2af6c4afd7e2e83d1ad880e035e98b2a89431d31
Abhinav Rathore <abhinavrathore9399@gmail.com>
2026-08-09 00:06:10 +0530

    Create 2550-WordsWithinTwoEditsOfDictionary

---
+++ https:/github.com/Abhinxvvvv/Leetcode.git/2550-WordsWithinTwoEditsOfDictionary/2550-WordsWithinTwoEditsOfDictionary.java
@@ -0,0 +1,32 @@
+// Last updated: 09/08/2026, 00:06:10
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<String> twoEditWords(String[] queries, String[] dictionary) {
+        List<String> ans = new ArrayList<>();
+        
+        for (String q : queries) {
+            for (String d : dictionary) {
+                int diff = 0;
+                for (int i = 0; i < q.length(); i++) {
+                    if (q.charAt(i) != d.charAt(i)) {
+                        diff++;
+                    }
+                    // Early exit if more than 2 edits are needed
+                    if (diff > 2) {
+                        break;
+                    }
+                }
+                
+                // If matched with 2 or fewer edits, add it and check the next query
+                if (diff <= 2) {
+                    ans.add(q);
+                    break; 
+                }
+            }
+        }
+        
+        return ans;
+    }
+}
