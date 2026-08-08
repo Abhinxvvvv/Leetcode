c23624af3069f438fb99fc89563ea2bf76ff97fd
Abhinav Rathore <abhinavrathore9399@gmail.com>
2026-08-09 00:05:18 +0530

    Create 3847-MinimumSwapsToSortByDigitSum

---
+++ https:/github.com/Abhinxvvvv/Leetcode.git/3847-MinimumSwapsToSortByDigitSum/3847-MinimumSwapsToSortByDigitSum.java
@@ -0,0 +1,54 @@
+// Last updated: 09/08/2026, 00:05:18
+import java.util.Arrays;
+
+class Solution {
+    public int minSwaps(int[] nums) {
+        int n = nums.length;
+        int[][] arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = nums[i];
+            arr[i][1] = i;
+        }
+
+        Arrays.sort(arr, (a, b) -> {
+            int sumA = getDigitSum(a[0]);
+            int sumB = getDigitSum(b[0]);
+            if (sumA != sumB) {
+                return Integer.compare(sumA, sumB);
+            }
+            return Integer.compare(a[0], b[0]);
+        });
+
+        boolean[] visited = new boolean[n];
+        int swaps = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (visited[i] || arr[i][1] == i) {
+                continue;
+            }
+
+            int cycleSize = 0;
+            int j = i;
+            while (!visited[j]) {
+                visited[j] = true;
+                j = arr[j][1];
+                cycleSize++;
+            }
+
+            if (cycleSize > 0) {
+                swaps += (cycleSize - 1);
+            }
+        }
+
+        return swaps;
+    }
+
+    private int getDigitSum(int num) {
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
+    }
+}
