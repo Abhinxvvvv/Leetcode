7101ed1fbd45eb0f57cba3799f45eadf9591ee3a
Abhinav Rathore <abhinavrathore9399@gmail.com>
2026-08-14 21:33:34 +0530

    Create 1240-StoneGameIi

---
+++ https:/github.com/Abhinxvvvv/Leetcode.git/1240-StoneGameIi/1240-StoneGameIi.java
@@ -0,0 +1,39 @@
+// Last updated: 14/08/2026, 21:33:34
+class Solution {
+    public int stoneGameII(int[] piles) {
+        int n = piles.length;
+        
+        // 1. In-place Suffix Sum: piles[i] becomes the sum of stones from i to the end
+        for (int i = n - 2; i >= 0; i--) {
+            piles[i] += piles[i + 1];
+        }
+        
+        // dp[i][M] represents the max stones a player can get starting at index i with M
+        int[][] dp = new int[n][n + 1];
+        
+        // 2. Bottom-up DP: Start from the end of the array and work backward
+        for (int i = n - 1; i >= 0; i--) {
+            for (int m = 1; m <= n; m++) {
+                // Base Case: If you can take all remaining piles, take them!
+                if (i + 2 * m >= n) {
+                    dp[i][m] = piles[i];
+                } else {
+                    int maxStones = 0;
+                    // Try all possible valid moves (1 to 2M)
+                    for (int x = 1; x <= 2 * m; x++) {
+                        int nextM = Math.max(m, x);
+                        // Maximize: (Total stones available) - (What the opponent will take)
+                        int currentStones = piles[i] - dp[i + x][nextM];
+                        if (currentStones > maxStones) {
+                            maxStones = currentStones;
+                        }
+                    }
+                    dp[i][m] = maxStones;
+                }
+            }
+        }
+        
+        // Alice starts at index 0 with M = 1
+        return dp[0][1];
+    }
+}
