94a76f2d40a3309e383a4b1e432d438c351b71e4
Abhinav Rathore <abhinavrathore9399@gmail.com>
2026-08-24 16:30:30 +0530

    Create 2002-StoneGameViii

---
+++ https:/github.com/Abhinxvvvv/Leetcode.git/2002-StoneGameViii/2002-StoneGameViii.java
@@ -0,0 +1,27 @@
+// Last updated: 24/08/2026, 16:30:30
+class Solution {
+    public int stoneGameVIII(int[] stones) {
+        int n = stones.length;
+        int currentPrefixSum = 0;
+        
+        // Calculate the total sum (which is prefix[n - 1])
+        for (int i = 0; i < n; i++) {
+            currentPrefixSum += stones[i];
+        }
+        
+        // Base case: at the very last possible move, the next player gets 0.
+        int maxDiff = currentPrefixSum;
+        
+        // Traverse backwards, stopping at 1 because x > 1 (must take at least 2 stones)
+        for (int i = n - 2; i >= 1; i--) {
+            currentPrefixSum -= stones[i + 1]; // Dynamically adjust to get prefix[i]
+            
+            // maxDiff = Math.max(maxDiff, currentPrefixSum - maxDiff)
+            if (currentPrefixSum - maxDiff > maxDiff) {
+                maxDiff = currentPrefixSum - maxDiff;
+            }
+        }
+        
+        return maxDiff;
+    }
+}
