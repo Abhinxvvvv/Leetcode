35d46aa49967cc9b472f97a5bc702448a545fa17
Abhinav Rathore <abhinavrathore9399@gmail.com>
2026-08-25 12:59:57 +0530

    Create 4080-SmallestMissingMultipleOfK

---
+++ https:/github.com/Abhinxvvvv/Leetcode.git/4080-SmallestMissingMultipleOfK/4080-SmallestMissingMultipleOfK.java
@@ -0,0 +1,21 @@
+// Last updated: 25/08/2026, 12:59:56
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+        // Flat array tracking (index matches the number, up to the max constraint of 100)
+        boolean[] present = new boolean[101];
+        
+        for (int i = 0; i < nums.length; i++) {
+            present[nums[i]] = true;
+        }
+        
+        int multiple = k;
+        
+        // Rapid lookup: exit if the multiple is over 100 (since max num is 100) 
+        // or if the multiple isn't found in our tracking array.
+        while (multiple <= 100 && present[multiple]) {
+            multiple += k;
+        }
+        
+        return multiple;
+    }
+}
