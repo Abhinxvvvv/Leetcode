b88d7a7efe10b622fb640d4533116e7db66712be
Abhinav Rathore <abhinavrathore9399@gmail.com>
2026-09-04 11:33:06 +0530

    Create 2540-MinimumCommonValue

---
+++ https:/github.com/Abhinxvvvv/Leetcode.git/2540-MinimumCommonValue/2540-MinimumCommonValue.java
@@ -0,0 +1,22 @@
+// Last updated: 04/09/2026, 11:33:05
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        int i = 0;
+        int j = 0;
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+        
+        // Linear scan utilizing raw primitive pointers
+        while (i < len1 && j < len2) {
+            if (nums1[i] == nums2[j]) {
+                return nums1[i]; // Instant early exit on the smallest match
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        
+        return -1; // No common intersection found
+    }
+}
