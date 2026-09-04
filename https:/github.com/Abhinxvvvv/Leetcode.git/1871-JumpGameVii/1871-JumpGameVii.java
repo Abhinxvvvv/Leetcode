4d4c921a0017bb93a6b48a37a4c8670ec6a50653
Abhinav Rathore <abhinavrathore9399@gmail.com>
2026-09-04 11:33:40 +0530

    Create 1871-JumpGameVii

---
+++ https:/github.com/Abhinxvvvv/Leetcode.git/1871-JumpGameVii/1871-JumpGameVii.java
@@ -0,0 +1,34 @@
+// Last updated: 04/09/2026, 11:33:39
+class Solution {
+    public boolean canReach(String s, int minJump, int maxJump) {
+        int n = s.length();
+        
+        // Quick short-circuit: if the last character is '1', we can never land on it
+        if (s.charAt(n - 1) == '1') return false;
+        
+        boolean[] dp = new boolean[n];
+        dp[0] = true; // Base case: starting position is always reachable
+        
+        int reachableCount = 0;
+        
+        for (int i = 1; i < n; i++) {
+            // 1. Add elements entering the sliding window from the right
+            if (i >= minJump && dp[i - minJump]) {
+                reachableCount++;
+            }
+            
+            // 2. Remove elements falling out of the sliding window from the left
+            if (i > maxJump && dp[i - maxJump - 1]) {
+                reachableCount--;
+            }
+            
+            // 3. If the window contains at least one reachable point and current index is '0'
+            if (reachableCount > 0 && s.charAt(i) == '0') {
+                dp[i] = true;
+            }
+        }
+        
+        return dp[n - 1];
+    }
+}
+
