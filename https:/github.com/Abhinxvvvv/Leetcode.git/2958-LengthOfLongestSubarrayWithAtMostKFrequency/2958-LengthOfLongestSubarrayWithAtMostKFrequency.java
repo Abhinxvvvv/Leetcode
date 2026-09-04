be555fe106b936b7d0d64214de8c9df32f5d6297
Abhinav Rathore <abhinavrathore9399@gmail.com>
2026-09-04 11:32:40 +0530

    Create 2958-LengthOfLongestSubarrayWithAtMostKFrequency

---
+++ https:/github.com/Abhinxvvvv/Leetcode.git/2958-LengthOfLongestSubarrayWithAtMostKFrequency/2958-LengthOfLongestSubarrayWithAtMostKFrequency.java
@@ -0,0 +1,36 @@
+// Last updated: 04/09/2026, 11:32:39
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int maxSubarrayLength(int[] nums, int k) {
+        int n = nums.length;
+        // Pre-size the HashMap to prevent slow resizing re-allocations
+        Map<Integer, Integer> freqs = new HashMap<>(n);
+        
+        int maxLen = 0;
+        int left = 0;
+        
+        for (int right = 0; right < n; right++) {
+            int val = nums[right];
+            // Extract and update the frequency efficiently
+            int count = freqs.getOrDefault(val, 0) + 1;
+            freqs.put(val, count);
+            
+            // If the current element exceeds k, shrink from the left
+            while (freqs.get(val) > k) {
+                int leftVal = nums[left];
+                freqs.put(leftVal, freqs.get(leftVal) - 1);
+                left++;
+            }
+            
+            // Calculate current window size
+            int currentLen = right - left + 1;
+            if (currentLen > maxLen) {
+                maxLen = currentLen;
+            }
+        }
+        
+        return maxLen;
+    }
+}
