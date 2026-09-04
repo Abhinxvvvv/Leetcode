f2f260e2ded47e673b376a30c4f2b2d43505764a
Abhinav Rathore <abhinavrathore9399@gmail.com>
2026-09-04 11:34:34 +0530

    Create 788-RotatedDigits

---
+++ https:/github.com/Abhinxvvvv/Leetcode.git/788-RotatedDigits/788-RotatedDigits.java
@@ -0,0 +1,35 @@
+// Last updated: 04/09/2026, 11:34:34
+class Solution {
+    public int rotatedDigits(int n) {
+        int count = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            int temp = i;
+            boolean isGood = false;
+            
+            while (temp > 0) {
+                int digit = temp % 10;
+                
+                // If it contains an invalid digit, it's permanently ruined. Break early.
+                if (digit == 3 || digit == 4 || digit == 7) {
+                    isGood = false;
+                    break;
+                }
+                
+                // If it contains a rotating digit, it becomes a candidate for a "Good" number.
+                // We DON'T break here, because a later digit might be a 3, 4, or 7!
+                if (digit == 2 || digit == 5 || digit == 6 || digit == 9) {
+                    isGood = true;
+                }
+                
+                temp /= 10;
+            }
+            
+            if (isGood) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
