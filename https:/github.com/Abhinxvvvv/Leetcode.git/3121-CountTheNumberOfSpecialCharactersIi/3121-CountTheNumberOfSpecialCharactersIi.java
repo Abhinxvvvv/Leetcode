b985b2ac8621ff22a56027df268d2f316c9998db
Abhinav Rathore <abhinavrathore9399@gmail.com>
2026-09-04 11:32:15 +0530

    Create 3121-CountTheNumberOfSpecialCharactersIi

---
+++ https:/github.com/Abhinxvvvv/Leetcode.git/3121-CountTheNumberOfSpecialCharactersIi/3121-CountTheNumberOfSpecialCharactersIi.java
@@ -0,0 +1,39 @@
+// Last updated: 04/09/2026, 11:32:15
+class Solution {
+    public int numberOfSpecialChars(String word) {
+        // Fast flat array lookups for 26 English characters
+        int[] lastLower = new int[26];
+        int[] firstUpper = new int[26];
+        
+        // Initialize with -1 to indicate unseen status
+        for (int i = 0; i < 26; i++) {
+            lastLower[i] = -1;
+            firstUpper[i] = -1;
+        }
+        
+        int len = word.length();
+        for (int i = 0; i < len; i++) {
+            char c = word.charAt(i);
+            if (c >= 'a' && c <= 'z') {
+                // Continuously overwrite to track the LAST lowercase position
+                lastLower[c - 'a'] = i;
+            } else {
+                int idx = c - 'A';
+                // Lock down only the FIRST uppercase position
+                if (firstUpper[idx] == -1) {
+                    firstUpper[idx] = i;
+                }
+            }
+        }
+        
+        int specialCount = 0;
+        // Verify conditions linearly in O(1) constant time
+        for (int i = 0; i < 26; i++) {
+            if (lastLower[i] != -1 && firstUpper[i] != -1 && lastLower[i] < firstUpper[i]) {
+                specialCount++;
+            }
+        }
+        
+        return specialCount;
+    }
+}
