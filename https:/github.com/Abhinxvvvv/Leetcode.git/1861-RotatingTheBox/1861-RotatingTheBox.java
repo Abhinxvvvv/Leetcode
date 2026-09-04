cbd083690eebf247e2e4c9a39759f4923429b7a7
Abhinav Rathore <abhinavrathore9399@gmail.com>
2026-09-04 11:33:45 +0530

    Create 1861-RotatingTheBox

---
+++ https:/github.com/Abhinxvvvv/Leetcode.git/1861-RotatingTheBox/1861-RotatingTheBox.java
@@ -0,0 +1,34 @@
+// Last updated: 04/09/2026, 11:33:44
+class Solution {
+    public char[][] rotateTheBox(char[][] boxGrid) {
+        int m = boxGrid.length;
+        int n = boxGrid[0].length;
+        
+        // 1. Apply gravity horizontally (stones fall to the right)
+        for (int i = 0; i < m; i++) {
+            int emptySpot = n - 1;
+            for (int j = n - 1; j >= 0; j--) {
+                if (boxGrid[i][j] == '*') {
+                    // Obstacle resets the lowest available drop point
+                    emptySpot = j - 1;
+                } else if (boxGrid[i][j] == '#') {
+                    // Move the stone to the empty spot. 
+                    // (If j == emptySpot, it briefly becomes '.', then instantly '#' again)
+                    boxGrid[i][j] = '.';
+                    boxGrid[i][emptySpot] = '#';
+                    emptySpot--;
+                }
+            }
+        }
+        
+        // 2. Map to a rotated N x M matrix
+        char[][] res = new char[n][m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                res[j][m - 1 - i] = boxGrid[i][j];
+            }
+        }
+        
+        return res;
+    }
+}
