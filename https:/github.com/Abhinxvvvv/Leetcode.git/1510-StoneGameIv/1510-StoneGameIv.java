ba6428fbf20b79092fcea22a71c502c78268a03e
Abhinav Rathore <abhinavrathore9399@gmail.com>
2026-09-04 11:34:07 +0530

    Create 1510-StoneGameIv

---
+++ https:/github.com/Abhinxvvvv/Leetcode.git/1510-StoneGameIv/1510-StoneGameIv.java
@@ -0,0 +1,22 @@
+// Last updated: 04/09/2026, 11:34:07
+class Solution {
+    public boolean winnerSquareGame(int n) {
+        // dp[i] represents whether the current player can win starting with i stones
+        boolean[] dp = new boolean[n + 1];
+        
+        // Build the game states bottom-up
+        for (int i = 1; i <= n; i++) {
+            // Try removing every possible perfect square less than or equal to i
+            for (int k = 1; k * k <= i; k++) {
+                // If this move leaves the opponent in a guaranteed losing state
+                if (!dp[i - k * k]) {
+                    dp[i] = true;
+                    break; // CRITICAL OPTIMIZATION: Instant short-circuit
+                }
+            }
+        }
+        
+        // Alice starts with n stones, so dp[n] is her win/loss outcome
+        return dp[n];
+    }
+}
